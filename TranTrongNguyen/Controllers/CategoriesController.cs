f2100e082ca4f870a93809aed9d6f5696d7ccd12
unknown <trongnguyenat@gmail.com>
2020-06-19 18:53:08 +0700

    first commit

---
+++ TranTrongNguyen/Controllers/CategoriesController.cs
@@ -0,0 +1,260 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using TranTrongNguyen.Models;
+using System.ComponentModel;
+using System.Linq.Dynamic;
+using System.Linq.Expressions;
+using System.Reflection;
+using PagedList;
+
+namespace TranTrongNguyen.Controllers
+{
+    public class CategoriesController : Controller
+    {
+        private CSDLEntities db = new CSDLEntities();
+        public class HttpParamActionAttribute : ActionNameSelectorAttribute
+        {
+            public override bool IsValidName(ControllerContext controllerContext, string actionName, MethodInfo methodInfo)
+            {
+                if (actionName.Equals(methodInfo.Name, StringComparison.InvariantCultureIgnoreCase))
+                    return true;
+
+                var request = controllerContext.RequestContext.HttpContext.Request;
+                return request[methodInfo.Name] != null;
+            }
+        }
+
+        [HttpGet]
+        // GET: /Link/
+        public ActionResult Index(int? size, int? page, string sortProperty, string sortOrder, string searchString)
+        {
+
+            if (sortOrder == "asc") ViewBag.sortOrder = "desc";
+            if (sortOrder == "desc") ViewBag.sortOrder = "";
+            if (sortOrder == "") ViewBag.sortOrder = "asc";
+            ViewBag.searchValue = searchString;
+            ViewBag.sortProperty = sortProperty;
+            ViewBag.page = page;
+
+
+            List<SelectListItem> items = new List<SelectListItem>();
+            items.Add(new SelectListItem { Text = "2", Value = "2" });
+            items.Add(new SelectListItem { Text = "5", Value = "5" });
+            items.Add(new SelectListItem { Text = "20", Value = "20" });
+            items.Add(new SelectListItem { Text = "25", Value = "25" });
+            items.Add(new SelectListItem { Text = "50", Value = "50" });
+            items.Add(new SelectListItem { Text = "100", Value = "100" });
+            items.Add(new SelectListItem { Text = "200", Value = "200" });
+
+
+            foreach (var item in items)
+            {
+                if (item.Value == size.ToString()) item.Selected = true;
+            }
+            ViewBag.size = items;
+            ViewBag.currentSize = size;
+
+
+            var properties = typeof(Category).GetProperties();
+            List<Tuple<string, bool, int>> list = new List<Tuple<string, bool, int>>();
+            foreach (var item in properties)
+            {
+                int order = 999;
+                var isVirtual = item.GetAccessors()[0].IsVirtual;
+
+                if (item.Name == "Name") order = 2;
+                if (item.Name == "Note") order = 1;
+
+                if (item.Name == "CategoryID") continue;
+                Tuple<string, bool, int> t = new Tuple<string, bool, int>(item.Name, isVirtual, order);
+                list.Add(t);
+            }
+            list = list.OrderBy(x => x.Item3).ToList();
+
+
+            foreach (var item in list)
+            {
+                if (!item.Item2)
+                {
+                    if (sortOrder == "desc" && sortProperty == item.Item1)
+                    {
+                        ViewBag.Headings += "<th><a href='?page=" + page + "&size=" + ViewBag.currentSize + "&sortProperty=" + item.Item1 + "&sortOrder=" +
+                            ViewBag.sortOrder + "&searchString=" + searchString + "'>" + item.Item1 + "<i class='fa fa-fw fa-sort-desc'></i></th></a></th>";
+                    }
+                    else if (sortOrder == "asc" && sortProperty == item.Item1)
+                    {
+                        ViewBag.Headings += "<th><a href='?page=" + page + "&size=" + ViewBag.currentSize + "&sortProperty=" + item.Item1 + "&sortOrder=" +
+                            ViewBag.sortOrder + "&searchString=" + searchString + "'>" + item.Item1 + "<i class='fa fa-fw fa-sort-asc'></a></th>";
+                    }
+                    else
+                    {
+                        ViewBag.Headings += "<th><a href='?page=" + page + "&size=" + ViewBag.currentSize + "&sortProperty=" + item.Item1 + "&sortOrder=" +
+                           ViewBag.sortOrder + "&searchString=" + searchString + "'>" + item.Item1 + "<i class='fa fa-fw fa-sort'></a></th>";
+                    }
+
+                }
+                else ViewBag.Headings += "<th>" + item.Item1 + "</th>";
+            }
+
+
+            var category = from l in db.Categories
+                           select l;
+
+
+            if (String.IsNullOrEmpty(sortProperty)) sortProperty = "CategoryID";
+
+
+            if (sortOrder == "desc") category = category.OrderBy(sortProperty + " desc");
+            else if (sortOrder == "asc") category = category.OrderBy(sortProperty);
+            else category = category.OrderBy("CategoryID");
+
+
+            if (!String.IsNullOrEmpty(searchString))
+            {
+                category = category.Where(s => s.Name.Contains(searchString));
+            }
+
+
+            page = page ?? 1;
+
+
+            int pageSize = (size ?? 5);
+
+            ViewBag.pageSize = pageSize;
+
+
+            int pageNumber = (page ?? 1);
+
+
+            int checkTotal = (int)(category.ToList().Count / pageSize) + 1;
+
+            if (pageNumber > checkTotal) pageNumber = checkTotal;
+
+
+            return View(category.ToPagedList(pageNumber, pageSize));
+        }
+
+        [HttpPost, HttpParamAction]
+        public ActionResult Reset()
+        {
+            ViewBag.searchValue = "";
+            Index(null, null, "", "", "");
+            return View();
+        }
+        // GET: Categories
+        public ActionResult Index()
+        {
+            return View(db.Categories.ToList());
+        }
+
+        // GET: Categories/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Category category = db.Categories.Find(id);
+            if (category == null)
+            {
+                return HttpNotFound();
+            }
+            return View(category);
+        }
+
+        // GET: Categories/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Categories/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "CategoryID,Name,Note")] Category category)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Categories.Add(category);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(category);
+        }
+
+        // GET: Categories/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Category category = db.Categories.Find(id);
+            if (category == null)
+            {
+                return HttpNotFound();
+            }
+            return View(category);
+        }
+
+        // POST: Categories/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "CategoryID,Name,Note")] Category category)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(category).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(category);
+        }
+
+        // GET: Categories/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Category category = db.Categories.Find(id);
+            if (category == null)
+            {
+                return HttpNotFound();
+            }
+            return View(category);
+        }
+
+        // POST: Categories/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Category category = db.Categories.Find(id);
+            db.Categories.Remove(category);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
